010c2992876f93f1e2822c618c98124a0b89ba24
Rupesh pradhan <merupeshpradhan@gmail.com>
2025-04-02 17:26:57 +0530

    utils code discussion

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,26 @@
+const asyncHandler = (requestHandler) => {
+  (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+  };
+};
+
+export { asyncHandler };
+
+/* Use Typing Types */
+
+// const asyncHandler = () => {};
+// const asyncHandler = (fun) => () => {};
+// const asyncHandler = (fun) => async () => {};
+
+/* One Types */
+
+// const asyncHandler = (fn) => async (req, res, next) => {
+//   try {
+//     await fn(req, res, next);
+//   } catch (error) {
+//     res.status(error.code || 500).json({
+//       success: false,
+//       message: error.message,
+//     });
+//   }
+// };
